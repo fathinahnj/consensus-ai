98d19131b7cf28dbd782c13b3ae498f942d7a5bc
Fathinah Nur Jannah <nurjannahfathinah@gmail.com>
2026-01-17 14:25:05 +0700

    fix: add issue_based_reasoning and authority_deference in attribute education_level

--- src/attributes/education.py
+++ src/attributes/education.py
@@ -1,14 +1,20 @@
 EDUCATION_LEVELS = {
     "PrimaryAndLower": {
         "ratio": 0.238,
-        "cognitive_complexity": 0.30
+        "cognitive_complexity": 0.30,
+        "issue_based_reasoning": 0.30,
+        "authority_deference": 0.70
     },
     "SecondaryLevel": {
         "ratio": 0.61,
-        "cognitive_complexity": 0.50
+        "cognitive_complexity": 0.50,
+        "issue_based_reasoning": 0.50,
+        "authority_deference": 0.50
     },
     "University": {
         "ratio": 0.148,
-        "cognitive_complexity": 0.75
+        "cognitive_complexity": 0.75,
+        "issue_based_reasoning": 0.75,
+        "authority_deference": 0.25
     }
 }
