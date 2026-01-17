a909c1a4503b91427cf1ebae3c451e7087bf2689
Fathinah Nur Jannah <nurjannahfathinah@gmail.com>
2026-01-17 14:15:46 +0700

    feat: add attribute income level for attribute setting

---
+++ src/attributes/income.py
@@ -0,0 +1,20 @@
+INCOME_LEVELS = {
+    "Poor": {
+        "ratio": 0.233,
+        "economic_anxiety": 0.78,
+        "welfare_salience": 0.80,
+        "status_quo_bias": 0.25
+    },
+    "Vulnerable": {
+        "ratio": 0.536,
+        "economic_anxiety": 0.65,
+        "welfare_salience": 0.68,
+        "status_quo_bias": 0.40
+    },
+    "MiddleUpper": {
+        "ratio": 0.139,
+        "economic_anxiety": 0.35,
+        "welfare_salience": 0.42,
+        "status_quo_bias": 0.65
+    }
+}
