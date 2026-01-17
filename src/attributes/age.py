3dbde8a43a400a369da838d9cbe058d14f56268c
Fathinah Nur Jannah <nurjannahfathinah@gmail.com>
2026-01-17 14:14:23 +0700

    feat: add attribute age for attribute setting

---
+++ src/attributes/age.py
@@ -0,0 +1,22 @@
+AGE_GROUPS = {
+    "GenZ": {
+        "ratio": 0.2494,
+        "digital_exposure": 0.48
+    },
+    "Millennial": {
+        "ratio": 0.3344,
+        "digital_exposure": 0.40
+    },
+    "GenX": {
+        "ratio": 0.2705,
+        "digital_exposure": 0.32
+    },
+    "Boomer": {
+        "ratio": 0.1308,
+        "digital_exposure": 0.22
+    },
+    "PreBoomer": {
+        "ratio": 0.0149,
+        "digital_exposure": 0.12
+    }
+}
