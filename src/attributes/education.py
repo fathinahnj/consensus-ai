f3a669e13e6a2eda03198e98f9b01dc2ac39681e
Fathinah Nur Jannah <nurjannahfathinah@gmail.com>
2026-01-17 14:15:10 +0700

    feat: add attribute education level for attribute setting

---
+++ src/attributes/education.py
@@ -0,0 +1,14 @@
+EDUCATION_LEVELS = {
+    "PrimaryAndLower": {
+        "ratio": 0.238,
+        "cognitive_complexity": 0.30
+    },
+    "SecondaryLevel": {
+        "ratio": 0.61,
+        "cognitive_complexity": 0.50
+    },
+    "University": {
+        "ratio": 0.148,
+        "cognitive_complexity": 0.75
+    }
+}
